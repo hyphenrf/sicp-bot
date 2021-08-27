3508c73d83764e6ddc0e4e0807f57a2bd7bf6f84
Haz <hyphens@pm.me>
2021-08-27 23:41:06 +0000

    Use dune-sites to handle data placement

--- init.ml
+++ init.ml
@@ -16,6 +16,15 @@ let auth = let module M = Serial.F (struct
 end) in M.auth
 
 let lines =
+  match
+    List.(Sites.Sites.stuff
+          |> filter_map
+              (fun d -> Filename.concat d quotes
+               |> fun f -> if Sys.file_exists f then Some f else None)
+          |> hd)
+  with
+     | exception Failure _ -> failwith "Quotes file doesn't exist"
+     | quotes ->
   let read = open_in quotes in
   let lines = array_of_lines read in
   close_in read; lines
